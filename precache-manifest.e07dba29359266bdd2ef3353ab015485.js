a7ae0933dde49004de9536c690c32f3a2323f6f7
olivertylsar <kendees@seznam.cz>
2019-12-05 10:40:29 +0100

    Updates

---
+++ precache-manifest.e07dba29359266bdd2ef3353ab015485.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a382ae66113294c365dd33c244a79394",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "177b5257c9a08b39ddb4",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0fd89f9c67906ea19a55",
+    "url": "/robofriends/static/css/main.9c0ccf23.chunk.css"
+  },
+  {
+    "revision": "177b5257c9a08b39ddb4",
+    "url": "/robofriends/static/js/2.c4c3b9a6.chunk.js"
+  },
+  {
+    "revision": "0fd89f9c67906ea19a55",
+    "url": "/robofriends/static/js/main.39644829.chunk.js"
+  },
+  {
+    "revision": "5645dfe01f57d087ff9e",
+    "url": "/robofriends/static/js/runtime-main.47a7d18e.js"
+  }
+]);
